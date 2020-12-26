d0f9bb2bb5d0aaf9f580a77fcbea61d11f908fd2
Sourabhvarma <61043049+Sourabhvarma@users.noreply.github.com>
2020-12-26 15:11:14 +0530

    servlet with web.xml
    learning

---
+++ webapp/src/com/webapp/AdderServlet.java
@@ -0,0 +1,46 @@
+package com.webapp;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+	//@WebServlet("/add")
+	public class AdderServlet extends HttpServlet 
+	{
+		public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
+		{
+			int i = Integer.parseInt(req.getParameter("num1"));
+			int j = Integer.parseInt(req.getParameter("num2"));
+			int k = i + j;
+			
+			System.out.println("testing 1234"+k);
+			
+//			PrintWriter out = res.getWriter();
+//			out.println("result is " + k);
+			
+			//req.setAttribute("k", k);
+			//RequestDispatcher rd = 	req.getRequestDispatcher("sq");
+			//rd.forward(req, res);
+			
+			PrintWriter out = res.getWriter();
+			out.println("Sum  Value is " + k);
+		}
+		
+		public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException
+		{
+			
+			doPost(req,res);
+		}
+		
+		
+		
+
+	}
+
+
